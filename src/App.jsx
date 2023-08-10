40b86c0a66f1035f3d31e4e5f20b25e7d9a40a46
italo <jau.batista@dcx.ufpb.br>
2023-08-09 23:10:33 -0300

    Tela login e cadastro criadas, mais detalhes futuro

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import { AppRouter } from './routes';
+import './assets/global.css'; // Certifique-se de ter o arquivo de estilos corretamente importado
+
+export const App = () => {
+  return (
+    <AppRouter />
+  );
+};
+
+
